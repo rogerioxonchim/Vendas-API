a5c8203442160bbb292dbe576b2c6fa3c864af89
Rogerio <rogerio@voti.com.br>
2023-04-10 15:47:34 -0300

    * Criando Dockerfile para subir o compras * Implementado no docker-compose.yml subir o rabbitmq * Implementado envio de e-mail no workercompras

--- workercompras/src/main/java/com/workercompras/consumer/Consumer.java
+++ workercompras/src/main/java/com/workercompras/consumer/Consumer.java
@@ -3,7 +3,9 @@ package com.workercompras.consumer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.workercompras.model.Pedido;
+import com.workercompras.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +14,19 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Component
 public class Consumer {
 
     private final ObjectMapper mapper;
+    private final EmailService emailService;
 
     @RabbitListener(queues = {"${queue.name}"})
     public void consumer(@Payload Message message) throws IOException {
         var pedido = mapper.readValue(message.getBody(), Pedido.class);
         System.out.println("Mensagem recebid no WorkerCompras: " + pedido);
+        emailService.notificarCliente("rogerio@voti.com.br");
     }
 
 }
