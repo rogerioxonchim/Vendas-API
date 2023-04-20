b71479608c23797c5051218608709d75ec33f982
Rogerio <rogerio@voti.com.br>
2023-04-20 10:02:11 -0300

    * (CI/CD) GitHub Actions

--- compra/src/test/java/com/mscompra/controller/PedidoControllerTest.java
+++ compra/src/test/java/com/mscompra/controller/PedidoControllerTest.java
@@ -79,7 +79,7 @@ public class PedidoControllerTest {
     @DisplayName("GET - Deve falhar ao buscar pedido que nao existe")
     @Test
     void deveFalharAoBuscarPedidoQueNaoExiste() throws Exception {
-        var id = 3L;
+        var id = 10L;
 
         mockMvc.perform(get(ROTA_PEDIDO.concat("/" + id)))
                 .andDo(print())
